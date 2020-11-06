c4d4163c5bcec99fe4b6a4b97eaa9825e832e764
pehleringer18 <pehleringer18@georgefox.edu>
2020-11-05 23:38:28 -0800

    more

---
+++ dynamic.js
@@ -0,0 +1,69 @@
+function replaceGods()
+{
+    location.replace("dynamicGods.html");
+}
+
+function replaceCreatures()
+{
+    location.replace("dynamicCreatures.html");
+}
+
+function showImageZeus()
+{
+    document.zeus.src="zeuz.jpg";
+}
+
+function showImageHera()
+{
+    document.zeus.src="hera.jpg";
+}
+
+function showImagePoseidon()
+{
+    document.zeus.src="poseidon.jpg";
+}
+
+function showImageDemeter()
+{
+    document.zeus.src="demeter.jpg";
+}
+
+function showImageAthena()
+{
+    document.zeus.src="athena.jpg";
+}
+
+function showImageApollo()
+{
+    document.zeus.src="apollo.png";
+}
+
+function showImageArtemis()
+{
+    document.zeus.src="artemis.jpg";
+}
+
+function showImageAres()
+{
+    document.zeus.src="ares.jpg";
+}
+
+function showImageHepheastus()
+{
+    document.zeus.src="hephaestus.jpg";
+}
+
+function showImageAphrodite()
+{
+    document.zeus.src="aphrodite.jpg";
+}
+
+function showImageHermes()
+{
+    document.zeus.src="hermes.jpg";
+}
+
+function showImageDionysus()
+{
+    document.zeus.src="dionysus.jpg";
+}
