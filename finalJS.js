8a1e58837b92bec9653d2c91af9bbd234c040fed
pehleringer18 <pehleringer18@georgefox.edu>
2020-12-02 13:44:59 -0800

    jsut some fun

--- finalJS.js
+++ finalJS.js
@@ -3,6 +3,7 @@ function getJoke()
   // get request to API
   $.get("https://official-joke-api.appspot.com/jokes/random", function(data)
   {
-    $("#joke").html(data.setup + "<br><br>" + "..." + "<br><br><br>" + data.punchline);
+    $("#setup").html(data.setup + "...");
+    $("#punchline").html(data.punchline);
   });
 }
