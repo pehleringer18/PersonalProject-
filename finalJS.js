fa9129f48448675fe2442ce012f2c63d8d4dd1f0
pehleringer18 <pehleringer18@georgefox.edu>
2020-12-02 15:52:14 -0800

    last edit

--- finalJS.js
+++ finalJS.js
@@ -3,7 +3,7 @@ function getJoke()
   // get request to API
   $.get("https://official-joke-api.appspot.com/jokes/random", function(data)
   {
-    $("#setup").html(data.setup + "...");
+    $("#setup").html(data.setup);
     $("#punchline").html(data.punchline);
   });
 }
