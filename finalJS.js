97b31e1e7a315f61386f94aed0c98b0ffde36c09
pehleringer18 <pehleringer18@georgefox.edu>
2020-12-02 11:03:15 -0800

    fixed it

--- finalJS.js
+++ finalJS.js
@@ -3,6 +3,6 @@ function getJoke()
   // get request to API
   $.get("https://official-joke-api.appspot.com/jokes/random", function(data)
   {
-    $(".joke").html(data.setup + "<br>" + "..." + "<br>" + data.punchline);
+    $("#joke").html(data.setup + "<br>" + "..." + "<br>" + data.punchline);
   });
 }
