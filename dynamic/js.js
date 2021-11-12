f77193d9906b160cf7ae60daa209c16649c2d962
pehleringer18 <pehleringer18@georgefox.edu>
2021-11-11 21:25:01 -0800

    edits

--- dynamic/js.js
+++ dynamic/js.js
@@ -12,3 +12,63 @@ $(function() {
       }
     });
 });
+
+$(function() {
+    $( "#draggable1" ).draggable();
+    
+    $( "#droppable1" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+          $('#counter').html(function(i, val) {
+            return val * 1 + 1;
+          });
+        }
+      });
+  });
+
+  $(function() {
+    $( "#draggable2" ).draggable();
+    
+    $( "#droppable2" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+          $('#counter').html(function(i, val) {
+            return val * 1 + 1;
+          });
+        }
+      });
+  });
+
+  $(function() {
+    $( "#draggable3" ).draggable();
+    
+    $( "#droppable3" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+          $('#counter').html(function(i, val) {
+            return val * 1 + 1;
+          });
+        }
+      });
+  });
+
+  $(function() {
+    $( "#draggable4" ).draggable();
+    
+    $( "#droppable4" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+          $('#counter').html(function(i, val) {
+            return val * 1 + 1;
+          });
+        }
+      });
+  });
