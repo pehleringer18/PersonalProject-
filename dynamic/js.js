d9d2602175e8add724f6b70472332b6eda833a40
pehleringer18 <pehleringer18@georgefox.edu>
2021-11-11 16:48:43 -0800

    adding

---
+++ dynamic/js.js
@@ -0,0 +1,14 @@
+$(function() {
+    $( "#draggable" ).draggable();
+    
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+          $('#counter').html(function(i, val) {
+            return val * 1 + 1;
+          });
+        }
+      });
+    });
