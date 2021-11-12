936ff8ce55270a8e397f3759549148e54bb894f9
pehleringer18 <pehleringer18@georgefox.edu>
2021-11-11 21:18:53 -0800

    Update js.js

--- dynamic/js.js
+++ dynamic/js.js
@@ -1,13 +1,14 @@
-
-    $( "#draggable" ).draggable();
-    
-    $( "#droppable" ).droppable({
-      drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-          $('#counter').html(function(i, val) {
-            return val * 1 + 1;
-          });
-        }
-      });
+$(function() {
+  $( "#draggable" ).draggable();
+  
+  $( "#droppable" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+        $('#counter').html(function(i, val) {
+          return val * 1 + 1;
+        });
+      }
+    });
+});
