b5fc7a2aaa06d5fe15e50bb6da17e9576f9f8e25
pehleringer18 <pehleringer18@georgefox.edu>
2021-12-01 14:53:25 -0800

    Update finalJS.js

--- finalProject/finalJS.js
+++ finalProject/finalJS.js
@@ -12,13 +12,11 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
 
-      const h1 = document.createElement('h1')
-      $("#setup").textContent = value
-    })
+    const h1 = document.createElement('h1')
+    $("#setup").textContent = value
   } else {
     const errorMessage = document.createElement('marquee')
     errorMessage.textContent = `Gah, it's not working!`
