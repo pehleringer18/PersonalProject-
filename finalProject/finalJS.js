e608ca5577c9134eedcddceab7a656c343d95fc2
pehleringer18 <pehleringer18@georgefox.edu>
2021-12-01 13:57:25 -0800

    Update finalJS.js

--- finalProject/finalJS.js
+++ finalProject/finalJS.js
@@ -3,7 +3,7 @@ function getJoke()
   // get request to API
   $.get("https://dad-jokes.p.rapidapi.com/random/joke", function(data)
   {
-    $("body.setup").html(data.setup);
-    $("body.punchline").html(data.punchline);
+    $("#setup").html(data.setup);
+    $("#punchline").html(data.punchline);
   });
 }
