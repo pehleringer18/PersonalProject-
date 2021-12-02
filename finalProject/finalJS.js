67d7ec594090c15d9179156aec3b8bbb209a6d3e
pehleringer18 <pehleringer18@georgefox.edu>
2021-12-01 20:06:54 -0800

    unsure

--- finalProject/finalJS.js
+++ finalProject/finalJS.js
@@ -1,8 +1,12 @@
 const app = document.getElementById('root')
 
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
+app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
@@ -15,11 +19,15 @@ request.onload = function () {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
       const p = document.createElement('p')
       movie.description = movie.description.substring(0, 300)
       p.textContent = `${movie.description}...`
 
       container.appendChild(card)
+      card.appendChild(h1)
       card.appendChild(p)
     })
   } else {
