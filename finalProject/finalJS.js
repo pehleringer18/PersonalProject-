f649e3e768b8a2f5dcf108dcc07c64cbe3a86413
pehleringer18 <pehleringer18@georgefox.edu>
2021-12-02 11:20:58 -0800

    want action to be on a button press, not on load

--- finalProject/finalJS.js
+++ finalProject/finalJS.js
@@ -1,3 +1,5 @@
+// Parts of the code were based off of the example on 
+// https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
 const app = document.getElementById('root')
 
 const logo = document.createElement('img')
@@ -9,26 +11,28 @@ container.setAttribute('class', 'container')
 app.appendChild(logo)
 app.appendChild(container)
 
-var request = new XMLHttpRequest()
-request.open('GET', 'https://api.chucknorris.io/jokes/random', true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    const card = document.createElement('div')
-    card.setAttribute('class', 'card')
+document.getElementById('enterButton').onclick = function() {
+  var request = new XMLHttpRequest()
+  request.open('GET', 'https://api.chucknorris.io/jokes/random', true)
+  request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+    if (request.status >= 200 && request.status < 400) {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
 
-    const p = document.createElement('p')
-    data.value = data.value.substring(0, 300)
-    p.textContent = `${data.value}...`
+      const p = document.createElement('p')
+      data.value = data.value.substring(0, 300)
+      p.textContent = `${data.value}...`
 
-    container.appendChild(card)
-    card.appendChild(p)
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
+      container.appendChild(card)
+      card.appendChild(p)
+    } else {
+      const errorMessage = document.createElement('marquee')
+      errorMessage.textContent = `Gah, it's not working!`
+      app.appendChild(errorMessage)
+    }
   }
-}
 
-request.send()
+  request.send()
+}
